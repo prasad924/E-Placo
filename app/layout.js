473b37a230ac39f1f3a9a2407e267b7cdc3ef042
prasad924 <reddyprasad.tech@gmail.com>
2025-05-25 00:15:13 +0530

    fix - Structure of the project

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "E-Placo",
   description: "A simple digital placement system for KMIT",
+  icons: {
+    icon: "logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
